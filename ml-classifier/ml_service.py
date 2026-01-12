16f45a134fb30dddd85d952a376248752813c7df
DevMaheshBatta <devmaheshbatta@gmail.com>
2026-01-12 12:41:47 +0530

    ml service gateway intergration

---
+++ ml-classifier/ml_service.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pickle
+
+app = FastAPI(title="ML Document Classifier")
+
+vectorizer, model = pickle.load(open("model.pkl", "rb"))
+
+class DocumentRequest(BaseModel):
+    text: str
+
+@app.get("/")
+def health():
+    return {"status": "ML Service Running"}
+
+@app.post("/predict")
+def predict(data: DocumentRequest):
+    X = vectorizer.transform([data.text])
+    prediction = model.predict(X)[0]
+    return {"document_type": prediction}
